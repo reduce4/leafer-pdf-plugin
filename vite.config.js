4a9843848b16914474a578a58f75f138b5a7971c
happysmile <812024380@qq.com>
2023-08-06 19:09:09 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,25 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()
   ],
-  rollupOptions: {
-    input: 'src/create.js',
-    output: {
-      format: 'esm',
-      dir: 'dist',
-    },
-    plugins: [
-      {
-        name: "custom-wasm-loader",
-        resolveId(source, importer) {
-          if (source === 'libmupdf.wasm') {
-            return { id: new URL('assets/wasm/libmupdf.wasm', importer).href, external: true };
+  build: {
+    rollupOptions: {
+      input: 'src/create.js',
+      output: {
+        format: 'esm',
+        dir: 'dist',
+      },
+      plugins: [
+        {
+          name: "custom-wasm-loader",
+          resolveId(source, importer) {
+            if (source === 'libmupdf.wasm') {
+              return { id: new URL('assets/wasm/libmupdf.wasm', importer).href, external: true };
+            }
+            return null;
           }
-          return null;
         }
-      }
-    ]
+      ]
+    },
   },
+
 });
